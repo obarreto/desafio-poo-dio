e46fc4bee6d4be2e9e6e4b929c556f2589ab48b2
Ozeias De Santana Barreto <ozbarreto@outlook.com>
2022-01-22 11:45:32 -0300

    Interligando classes de domínio

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -6,25 +6,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Curso curso1 = new Curso();
+		Conteudo curso1 = new Curso();
 		curso1.setTitulo("Curso Java");
 		curso1.setDescricao("descrição curso java");
-		curso1.setCargaHoraria(8);
+		((Curso)curso1).setCargaHoraria(8);
 	
-		Curso curso2 = new Curso();
+		Conteudo curso2 = new Curso();
 		curso2.setTitulo("Curso Spring");
 		curso2.setDescricao("descrição curso Spring");
-		curso2.setCargaHoraria(6);
-	
-
-		Mentoria mentoria1 = new Mentoria();
+		((Curso)curso2).setCargaHoraria(6);
+		
+		
+		Conteudo mentoria1 = new Mentoria();
 		mentoria1.setTitulo("mentoria java");
 		mentoria1.setDescricao("descrição mentoria java");
-		mentoria1.setData(LocalDate.now());
+		((Mentoria) mentoria1).setData(LocalDate.now());
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria1);
+//		System.out.println(curso1);
+//		System.out.println(curso2);
+//		System.out.println(mentoria1);
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		Dev devOzeias = new Dev();
+		devOzeias.setNome("Ozeias");
+		devOzeias.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos" + devOzeias.getConteudosInscritos());
+		devOzeias.progredir();
+		devOzeias.progredir();
+		System.out.println("Conteúdos Inscritos" + devOzeias.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos" + devOzeias.getConteudosConcluidos());
+		System.out.println("XP: " + devOzeias.calculaXp());
 	}
 
 }
